eaae2aace6031850d3bc61736543c5bec0a68d4a
veroniquex1 <vdrossouw@gmail.com>
2023-12-13 14:11:59 +0200

    html and buttons added

---
+++ script/code.js
@@ -0,0 +1,5 @@
+let convertBtn = document.getElementById('convert') 
+convertBtn.addEventListener('click', convert)
+
+let clearBtn =document.getElementById("clear")
+clearBtn.addEventListener('click', delete)
